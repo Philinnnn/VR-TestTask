a4150a2b8d5a0202e765060a0a67b903958e3e46
Philin <armanela@bk.ru>
2026-09-03 14:46:17 +0500

    feat: add OnTriggerExit method

--- Assets/_Project/Scripts/Interactions/ZoneTarget.cs
+++ Assets/_Project/Scripts/Interactions/ZoneTarget.cs
@@ -5,12 +5,21 @@ namespace _Project.Scripts.Interactions
     [RequireComponent(typeof(Collider))]
     public class ZoneTarget : BaseInteractable
     {
+        private bool _playerInside;
+
         private void OnTriggerEnter(Collider other)
         {
+            if (_playerInside) return;
+            if (!other.CompareTag("Player")) return;
+
+            _playerInside = true;
+            TriggerInteraction();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
             if (other.CompareTag("Player"))
-            {
-                TriggerInteraction();
-            }
+                _playerInside = false;
         }
     }
 }
